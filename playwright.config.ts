3ae5aa9379a5f14487ec42278cc435d65eb9d394
DimaSmaha <dmytrosmaha@gmail.com>
2025-10-18 14:42:32 +0300

    upd config to pass all results under one folder

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,7 @@ import { defineConfig, devices } from "@playwright/test";
  */
 import dotenv from "dotenv";
 import path from "path";
+
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 /**
@@ -13,8 +14,9 @@ dotenv.config({ path: path.resolve(__dirname, ".env") });
  */
 export default defineConfig({
   testDir: "./playwright/tests",
+  testMatch: "*.spec.ts",
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -22,16 +24,28 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["html", { open: "never", outputFolder: "./playwright/reports/" }]],
+  outputDir: "./playwright/test-results",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  timeout: 1_200_000, // 1200 seconds = 20 minutes
+  expect: {
+    timeout: 40_000,
+  },
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
     baseURL: process.env.BASE_URL,
-    testIdAttribute: "data-test",
-
+    actionTimeout: 40_000,
+    navigationTimeout: 40_000,
+    ignoreHTTPSErrors: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+    launchOptions: {
+      args: ["--disable-web-security"],
+    },
+    testIdAttribute: "data-test",
+    ...devices["Desktop Chrome"],
+    viewport: { width: 1920, height: 1080 },
+    headless: true,
   },
 
   /* Configure projects for major browsers */
@@ -41,15 +55,15 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
 
     /* Test against mobile viewports. */
     // {
